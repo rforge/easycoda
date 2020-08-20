f2cbba41db1fd3b218d633dd00306760ef595628
mgreenacre <mgreenacre@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-08-20 15:21:28 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/easycoda@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/easycoda/R/PCA.r
+++ pkg/easycoda/R/PCA.r
@@ -1,18 +1,19 @@
 PCA <- function(data, nd = 2, weight = TRUE, row.wt = NA, suprow = NA) {
-# data          interval-scaled data table (e.g. logratios)
-# nd            number of dimensions for summary output, 2 by default
-# weight        = TRUE (default) when weights are in logratio list object
-#               = FALSE for unweighted analysis
-#               = vector of user-specified column weights  
-# row.wt        optional weights for rows 
-# suprow        columns of supplementary (passive) rows
+  # data          interval-scaled data table (e.g. logratios)
+  # nd            number of dimensions for summary output, 2 by default
+  # weight        = TRUE (default) when weights are in logratio list object
+  #               = FALSE for unweighted analysis
+  #               = vector of user-specified column weights
+  # row.wt        optional weights for rows
+  # suprow        columns of supplementary (passive) rows
 
-# 15/5/2019  fixed bug in supplementary row option, disabled supcol since it makes little sense for CoDa
+  # 15/5/2019  fixed bug in supplementary row option, disabled supcol since it makes little sense for CoDa
+  # 07/02/2020 fixed bug in row.wt definition
 
-# first check if data is a dataframe, if so make it a matrix
+  # first check if data is a dataframe, if so make it a matrix
   if(is.data.frame(data)) data <- as.matrix(data)
-   
-# then check if data is a list
+
+  # then check if data is a list
   foo <- data
   data.wt <- 0
   if(is.list(foo)) {
@@ -20,11 +21,11 @@ PCA <- function(data, nd = 2, weight = TRUE, row.wt = NA, suprow = NA) {
     data.wt <- foo$LR.wt
   }
 
-# do PCA
+  # do PCA
   X    <- as.matrix(data)
   Xact <- X
   if(!is.na(suprow[1])) { Xact <- X[-suprow,] }
-#### option omitted  if(!is.na(supcol[1])) { Xact <- X[,-supcol] }
+  #### option omitted  if(!is.na(supcol[1])) { Xact <- X[,-supcol] }
 
   if(!weight[1]) cm <- rep(1/ncol(Xact), ncol(Xact))
   if(length(weight) > 1) {
@@ -35,26 +36,26 @@ PCA <- function(data, nd = 2, weight = TRUE, row.wt = NA, suprow = NA) {
   if(length(data.wt) == ncol(data)) cm <- data.wt / sum(data.wt)
   if((as.numeric(weight[1]) == 1) & (!is.list(foo))) stop("weight = TRUE but data is not a logratio list object with weights")
 
-# row masses are equal unless specified
-# row masses of supplementary points are 0
+  # row masses are equal unless specified
+  # row masses of supplementary points are 0
   rm <- rep(0, nrow(X))
   if(is.na(suprow[1]))  {
-     rm <- rep(1/nrow(X), nrow(X)) 
-     rmact <- rm
+    rm <- rep(1/nrow(X), nrow(X))
+    rmact <- rm
   }
   if(!is.na(suprow[1])) {
-     rm[(1:nrow(X))[-suprow]] <- rep(1/nrow(Xact), nrow(Xact))
-     rmact <- rm[-suprow]
+    rm[(1:nrow(X))[-suprow]] <- rep(1/nrow(Xact), nrow(Xact))
+    rmact <- rm[-suprow]
   }
- 
-# for specified weights, a list with as many rows as data must be supplied (incl. 0s for suprow) 
+
+  # for specified weights, a list with as many rows as data must be supplied (incl. 0s for suprow)
   if(!is.na(row.wt[1])) {
     if(length(row.wt) != nrow(data)) stop("Number of specified row weights not equal to number of rows of data")
     if(sum(row.wt < 0) > 0) stop("Row weights have to be all nonnegative")
-    if(!is.na(suprow[1]) & (sum(row.wt[suprow]!=0)>0)) stop("Supplementary row weights should be 0") 
+    if(!is.na(suprow[1]) & (sum(row.wt[suprow]!=0)>0)) stop("Supplementary row weights should be 0")
     rm <- rep(0, nrow(X))
-    if(is.na(suprow[1]))  rm <- row.wt / sum(row.wt)
-    if(!is.na(suprow[1])) rm[(1:nrow(X))[-suprow]] <- row.wt / sum(row.wt)
+    if(is.na(suprow[1]))  rmact <- row.wt / sum(row.wt)
+    if(!is.na(suprow[1])) rmact[(1:nrow(X))[-suprow]] <- row.wt / sum(row.wt)
   }
   Y    <- Xact
   mc   <- t(Y) %*% as.vector(rmact)
@@ -66,46 +67,46 @@ PCA <- function(data, nd = 2, weight = TRUE, row.wt = NA, suprow = NA) {
   data.rsc <- (diag(1/sqrt(rmact)) %*% svdZ$u)[,1:ndmax]
   data.csc <- (diag(1/sqrt(cm)) %*% svdZ$v)[,1:ndmax]
 
-# supplementary rows but no supplementary columns (latter option non-existent)
-#  if(!is.na(suprow[1]) & is.na(supcol[1])) { 
-  if(!is.na(suprow[1])) { 
+  # supplementary rows but no supplementary columns (latter option non-existent)
+  #  if(!is.na(suprow[1]) & is.na(supcol[1])) {
+  if(!is.na(suprow[1])) {
 
-# supplementary rows; mc is the mean column vector of the active set
+    # supplementary rows; mc is the mean column vector of the active set
     Ysup     <- as.matrix(X[suprow,])
     if(length(suprow)==1) Ysup <- matrix(Ysup - mc, nrow=1)
     if(length(suprow)>1)  Ysup     <- Ysup - rep(1, nrow(Ysup)) %*% t(mc)
-    Ysup.rsc <- Ysup %*% diag(sqrt(cm)) %*% svdZ$v[,1:ndmax] %*% diag(1/svdZ$d[1:ndmax]) 
+    Ysup.rsc <- Ysup %*% diag(sqrt(cm)) %*% svdZ$v[,1:ndmax] %*% diag(1/svdZ$d[1:ndmax])
 
-# insert supplementary row information in correct places
-    foo           <- matrix(0, nrow = nrow(data), ncol = ndmax) 
+    # insert supplementary row information in correct places
+    foo           <- matrix(0, nrow = nrow(data), ncol = ndmax)
     foo[-suprow,] <- data.rsc
-    foo[suprow,]  <- Ysup.rsc 
+    foo[suprow,]  <- Ysup.rsc
     data.rsc      <- foo
   }
 
-#### supplementary columns but no supplementary rows
-###  if(is.na(suprow[1]) & !is.na(supcol[1])) { 
+  #### supplementary columns but no supplementary rows
+  ###  if(is.na(suprow[1]) & !is.na(supcol[1])) {
 
-### supplementary columns, work with matrix transpose
-###    Ysup     <- t(as.matrix(Xact[,supcol]))
-###    Ysup     <- Ysup - rep(1, nrow(Ysup)) %*% t(mr)
-###    mc       <- Ysup %*% as.vector(rm)
-###    Ysup     <- Ysup - mc %*% t(rep(1, ncol(Ysup)))
-###    Ysup.csc <- Ysup %*% diag(sqrt(rm)) %*% svdZ$u[,1:ndmax] %*% diag(1/svdZ$d[1:ndmax]) 
+  ### supplementary columns, work with matrix transpose
+  ###    Ysup     <- t(as.matrix(Xact[,supcol]))
+  ###    Ysup     <- Ysup - rep(1, nrow(Ysup)) %*% t(mr)
+  ###    mc       <- Ysup %*% as.vector(rm)
+  ###    Ysup     <- Ysup - mc %*% t(rep(1, ncol(Ysup)))
+  ###    Ysup.csc <- Ysup %*% diag(sqrt(rm)) %*% svdZ$u[,1:ndmax] %*% diag(1/svdZ$d[1:ndmax])
 
-#### insert supplementary row information in correct places
-###    foo           <- matrix(0, nrow = ncol(data), ncol = ndmax) 
-###    foo[-supcol,] <- data.csc
-###    foo[supcol,]  <- Ysup.csc 
-###    data.csc      <- foo
-###  }
+  #### insert supplementary row information in correct places
+  ###    foo           <- matrix(0, nrow = ncol(data), ncol = ndmax)
+  ###    foo[-supcol,] <- data.csc
+  ###    foo[supcol,]  <- Ysup.csc
+  ###    data.csc      <- foo
+  ###  }
 
-# principal coordinates
+  # principal coordinates
   data.rpc <- data.rsc %*% diag(svdZ$d[1:ndmax])
   data.cpc <- data.csc %*% diag(svdZ$d[1:ndmax])
-  
-# substitute results in ca object - first make dummy run
-  data.pca             <- ca(abs(X+1), suprow=suprow)  
+
+  # substitute results in ca object - first make dummy run
+  data.pca             <- ca(abs(X+1), suprow=suprow)
   data.pca$nd          <- nd
   data.pca$rowsup      <- suprow
   data.pca$colsup      <- NA
@@ -123,5 +124,4 @@ PCA <- function(data, nd = 2, weight = TRUE, row.wt = NA, suprow = NA) {
   data.pca$N           <- data
   data.pca$call        <- match.call()
   return(data.pca)
-  }
-
+}
